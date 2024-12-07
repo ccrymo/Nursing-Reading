2cc9ef2b825f62b5c703ca49505517b45f130235
Kirill <kkrymov.asc@gmail.com>
2024-12-08 02:18:21 +0300

    main manu colour change

--- app/page.js
+++ app/page.js
@@ -10,18 +10,18 @@ const Modal = () => {
         <h2 className="text-3xl font-bold text-white mb-6">Choose the slide</h2>
         <div className="flex flex-col space-y-4 ">
           <Link href="./components/Vocabulary">
-            <button className="w-full  text-2xl font-bold py-3 px-6 rounded-lg bg-gradient-to-r from-neutral-800 to-neutral-900 hover:from-lime-400 hover:to-lime-600 hover:text-lime-950 text-white">
+            <button className="w-full  text-2xl font-bold py-3 px-6 rounded-lg bg-gradient-to-r from-neutral-800 to-neutral-900 hover:from-amber-400 hover:to-amber-600 hover:text-amber-950 text-white">
               Vocabulary
             </button>
           </Link>
           <Link href="./components/Reading_Exam">
-            <button className="w-full text-2xl font-bold py-3 px-6 rounded-lg bg-gradient-to-r from-neutral-800 to-neutral-900 hover:from-lime-400 hover:to-lime-600 hover:text-lime-950 text-white">
+            <button className="w-full text-2xl font-bold py-3 px-6 rounded-lg bg-gradient-to-r from-neutral-800 to-neutral-900 hover:from-amber-400 hover:to-amber-600 hover:text-amber-950 text-white">
               Exam
             </button>
           </Link>
        
             <Link href="./components/Reading_Skills">
-              <button className="w-full text-2xl font-bold py-3 px-6 rounded-lg bg-gradient-to-r from-neutral-800 to-neutral-900 hover:from-lime-400 hover:to-lime-600 hover:text-lime-950 text-white">
+              <button className="w-full text-2xl font-bold py-3 px-6 rounded-lg bg-gradient-to-r from-neutral-800 to-neutral-900 hover:from-amber-400 hover:to-amber-600 hover:text-amber-950 text-white">
                 Skills overview
               </button>
             </Link>
